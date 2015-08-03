ef0f55f9a4fe128651379a8a2b8733641cda151d
PatoTheBest <jpguereque@yahoo.com.mx>
2015-08-03 13:11:18 -0500

    a

--- src/main/java/com/lastabyss/carbon/items/ItemArrow.java
+++ src/main/java/com/lastabyss/carbon/items/ItemArrow.java
@@ -2,11 +2,13 @@ package com.lastabyss.carbon.items;
 
 import net.minecraft.server.v1_8_R3.*;
 
-/**
- * @author Navid
- */
 public class ItemArrow extends Item {
+	
     public ItemArrow() {
         c("arrow");
     }
+    
+    public EntityArrow a(World var1, ItemStack var2, EntityLiving var3) {
+        return new EntityArrow(var1, var3.locX, var3.locY, var3.locZ);
+     }
 }
--- src/main/java/com/lastabyss/carbon/items/ItemSpectralArrow.java
+++ src/main/java/com/lastabyss/carbon/items/ItemSpectralArrow.java
@@ -1,11 +1,19 @@
 package com.lastabyss.carbon.items;
 
-/**
- * @author Navid
- */
+import com.lastabyss.carbon.entities.EntitySpectralArrow;
+
+import net.minecraft.server.v1_8_R3.EntityArrow;
+import net.minecraft.server.v1_8_R3.EntityLiving;
+import net.minecraft.server.v1_8_R3.ItemStack;
+import net.minecraft.server.v1_8_R3.World;
+
 public class ItemSpectralArrow extends ItemArrow {
 
     public ItemSpectralArrow() {
         c("spectral_arrow");
     }
+    
+    public EntityArrow a(World var1, ItemStack var2, EntityLiving var3) {
+        return new EntitySpectralArrow(var1, var3.locX, var3.locY, var3.locZ);
+     }
 }
