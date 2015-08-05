8ee42c385247be741b1943b0b4c1b580cb3169b7
Shevchik <Shev4ik.den@gmail.com>
2015-08-05 20:59:25 +0400

    Cleanup watched entities

--- src/main/java/com/lastabyss/carbon/network/watchedentity/WatchedLiving.java
+++ src/main/java/com/lastabyss/carbon/network/watchedentity/WatchedLiving.java
@@ -1,91 +1,39 @@
 package com.lastabyss.carbon.network.watchedentity;
 
+import java.util.Arrays;
+
 public class WatchedLiving extends WatchedEntity {
 
-    private boolean isLiving; //Yes, we do have a flag for living because some people tend to spawn objects using spawn mob packet (hologramm plugins most of the time)
+    private static final SpecificType[] specificTypes = new SpecificType[256];
+    static {
+        Arrays.fill(specificTypes, SpecificType.NONE);
+        specificTypes[100] = SpecificType.HORSE;
+        specificTypes[120] = SpecificType.VILLAGER;
+    }
+
     private boolean isAgeable;
     private boolean isTameable;
     private SpecificType stype;
 
     public WatchedLiving(int id, int type) {
         super(id);
-        stype = SpecificType.NONE;
-        //TODO: cleanup this shit somehow
+        stype = specificTypes[type];
         switch (type) {
-            case 50:
-            case 51:
-            case 52:
-            case 53:
-            case 54:
-            case 55:
-            case 56:
-            case 57:
-            case 58:
-            case 59:
-            case 60:
-            case 61:
-            case 62:
-            case 63:
-            case 64:
-            case 65:
-            case 66:
-            case 67:
-            case 68: {
-                isLiving = true;
-                break;
-            }
             case 90:
             case 91:
             case 92:
-            case 93: {
+            case 93:
+            case 96:
+            case 100:
+            case 101:
+            case 120: {
                 isAgeable = true;
-                isLiving = true;
                 break;
             }
-            case 94: {
-                isLiving = true;
-                break;
-            }
-            case 95: {
-                isTameable = true;
-                isAgeable = true;
-                isLiving = true;
-                break;
-            }
-            case 96: {
-                isAgeable = true;
-                isLiving = true;
-                break;
-            }
-            case 97: {
-                isLiving = true;
-                break;
-            }
+            case 95:
             case 98: {
                 isTameable = true;
                 isAgeable = true;
-                isLiving = true;
-                break;
-            }
-            case 99: {
-                isLiving = true;
-                break;
-            }
-            case 100: {
-                stype = SpecificType.HORSE;
-                isAgeable = true;
-                isLiving = true;
-                break;
-            }
-            case 101: {
-                isAgeable = true;
-                isLiving = true;
-                break;
-            }
-            case 120: {
-                stype = SpecificType.VILLAGER;
-                isAgeable = true;
-                isLiving = true;
                 break;
             }
         }
@@ -93,7 +41,7 @@ public class WatchedLiving extends WatchedEntity {
 
     @Override
     public boolean isLiving() {
-        return isLiving;
+        return true;
     }
 
     @Override
