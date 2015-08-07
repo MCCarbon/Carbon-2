b47287b19e8eb7c618e9dbd024fc8f28fa6d8f61
Shevchik <Shev4ik.den@gmail.com>
2015-08-07 22:57:50 +0400

    Finish datawatcher remapping table for old entities

--- src/main/java/com/lastabyss/carbon/network/watchedentity/SpecificType.java
+++ src/main/java/com/lastabyss/carbon/network/watchedentity/SpecificType.java
@@ -7,12 +7,12 @@ import org.bukkit.entity.EntityType;
 
 public enum SpecificType {
 
-    // TODO: more types
     NONE(EType.NONE, -1),
     LIVING(EType.NONE, -1, RemappingEntry.of(6, 6, "<>", 7, 7, "<>", 8, 8, "<>", 9, 9, "<>", 15, 10)), //health, potion color, potion ambient, arrows, no-ai
     PLAYER(EType.NONE, -1, SpecificType.LIVING, RemappingEntry.of(17, 10, "<>", 18, 11, "<>", 10, 12)), //absorption hearts, score, skinflags
     AGEABLE(EType.NONE, -1, SpecificType.LIVING, new RemappingEntry(12, 11)), //age
     TAMEABLE(EType.NONE, -1, SpecificType.AGEABLE, new RemappingEntry(16, 12), new RemappingEntry(17, 13)), //tame flags, owner
+    ARMOR_STAND(EType.NONE, -1, SpecificType.LIVING, RemappingEntry.ofOriginal(10, 16)), //info, head, body, left arm, right arm, left leg, right leg
     HORSE(EType.MOB, EntityType.HORSE, SpecificType.AGEABLE, RemappingEntry.of(16, 12, "<>", 19, 13, "<>", 20, 14, "<>", 21, 15, "<>", 22, 16)), //info flags, type, color/variant, owner, armor
     BAT(EType.MOB, EntityType.BAT, SpecificType.LIVING, new RemappingEntry(16, 11)), //hanging
     OCELOT(EType.MOB, EntityType.OCELOT, SpecificType.TAMEABLE, new RemappingEntry(18, 14)), //type
@@ -36,8 +36,15 @@ public enum SpecificType {
     IRON_GOLEM(EType.MOB, EntityType.IRON_GOLEM, SpecificType.LIVING, new RemappingEntry(16, 11)), //player created
     WITHER(EType.MOB, EntityType.WITHER, SpecificType.LIVING, RemappingEntry.ofShift(17, 20, 11)), //target 1-3, invulnerable time
     GUARDIAN(EType.MOB, EntityType.GUARDIAN, SpecificType.LIVING, new RemappingEntry(16, 11), new RemappingEntry(17, 12)), //info flags(elder, spikes), target id
+    ARMOR_STAND_MOB(EType.MOB, EntityType.ARMOR_STAND, SpecificType.ARMOR_STAND),
+    BOAT(EType.OBJECT, 1, RemappingEntry.ofShift(17, 19, 5)), //time since hit, forward direction, damage taken
     ITEM(EType.OBJECT, 2, new RemappingEntry(10, 5)), //item
-    ITEM_FRAME(EType.OBJECT, 71, new RemappingEntry(8, 5), new RemappingEntry(9, 6)); //item, rotation
+    MINECART(EType.OBJECT, 10, combine(RemappingEntry.ofShift(17, 22, 5), new RemappingEntry[] { new RemappingEntry(16, 11) })), //shaking power, shaking direction, damage taken, block, block y, show block, is powered
+    ARROW(EType.OBJECT, 60, new RemappingEntry(16, 5)), //is critical
+    FIREWORK(EType.OBJECT, 76, new RemappingEntry(8, 5)), //info
+    ITEM_FRAME(EType.OBJECT, 71, new RemappingEntry(8, 5), new RemappingEntry(9, 6)), //item, rotation
+    ENDER_CRYSTAL(EType.OBJECT, 51, new RemappingEntry(8, 5)), //health
+    ARMOR_STAND_OBJECT(EType.OBJECT, 78, SpecificType.ARMOR_STAND);
 
     private static final SpecificType[] OBJECT_BY_TYPE_ID = new SpecificType[256];
     private static final SpecificType[] MOB_BY_TYPE_ID = new SpecificType[256];
@@ -99,20 +106,6 @@ public enum SpecificType {
     }
 
     public static class RemappingEntry {
-        private int from;
-        private int to;
-        private RemappingEntry(int from, int to) {
-            this.from = from;
-            this.to = to;
-        }
-
-        public int getFrom() {
-            return from;
-        }
-
-        public int getTo() {
-            return to;
-        }
 
         protected static RemappingEntry[] of(Object... pairsWithSeparator) {
             ArrayList<RemappingEntry> entries = new ArrayList<RemappingEntry>();
@@ -128,6 +121,25 @@ public enum SpecificType {
                 entries.add(new RemappingEntry(i, to++));
             }
             return entries.toArray(new RemappingEntry[entries.size()]);
+        }
+        
+        public static RemappingEntry[] ofOriginal(int fromBegin, int fromEnd) {
+            return ofShift(fromBegin, fromEnd, fromBegin);
+        }
+
+        private int from;
+        private int to;
+        private RemappingEntry(int from, int to) {
+            this.from = from;
+            this.to = to;
+        }
+
+        public int getFrom() {
+            return from;
+        }
+
+        public int getTo() {
+            return to;
         }
 
         @Override
